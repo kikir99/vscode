3a1e27bb9707dde2a1fb13a7399f72be602e9ee2
Felixxx96 <muhammaddwifahkrixrpl@gmail.com>
2025-08-09 13:00:31 +0700

    coba

---
+++ Praktikum14 copy.py
@@ -0,0 +1,9 @@
+import random
+
+angka_rahasia = random.randint(1, 100)
+ronde = 0
+max_ronde = 5
+
+print("===== TEBAK ANGKA =====")
+print("Saya telah memilih sebuah angka antara 1 dan 100.")
+print("Kamu tebak angka tersebut dalam 5 ronde.")
